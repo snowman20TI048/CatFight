7f1de9897671048b234a9d32a536e9c7ca96dff4
snowman20TI048 <snowman2000y@gmail.com>
2022-12-17 14:26:34 +0900

    肉の親子関係を修正中。

--- Assets/Scripts/Radar.cs
+++ Assets/Scripts/Radar.cs
@@ -7,6 +7,8 @@ public class Radar : MonoBehaviour
     [SerializeField]
     private Transform target;
 
+    private bool isChild = false;
+
     // 「OnTriggerStay」はトリガーが他のコライダーに触れている間中実行されるメソッド（ポイント）
     private void OnTriggerStay(Collider other)
     {
@@ -15,13 +17,41 @@ public class Radar : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.X))
             {
-                this.gameObject.GetComponent<Rigidbody>().useGravity = false;
-                this.gameObject.GetComponent<Rigidbody>().isKinematic = false;
-                this.gameObject.GetComponent<CapsuleCollider>().enabled = false;
-                this.gameObject.transform.parent = other.gameObject.transform;
-                this.gameObject.transform.localPosition = new Vector3(0f, 0.07f, 0f);
-               
+                if (isChild == false)
+                {
+                    Debug.Log("a");
+                    this.gameObject.GetComponent<Rigidbody>().useGravity = false;
+                    this.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                    this.gameObject.GetComponent<CapsuleCollider>().enabled = false;
+                    this.gameObject.transform.parent = other.gameObject.transform;
+                    this.gameObject.transform.localPosition = new Vector3(0f, 0.07f, 0f);
+                    this.gameObject.GetComponent<SphereCollider>().enabled = false;
+                    isChild = true;
+                }
+
+           
             }
+
         }
     }
+
+    private void Update()
+    {
+        if(isChild == false)
+        {
+            return;
+        }
+        if(Input.GetKeyDown(KeyCode.X) && isChild == true)
+        {
+            Debug.Log("b");
+            isChild = false;
+            this.gameObject.transform.localPosition = new Vector3(0f, 0.07f, 0.07f);
+            this.gameObject.transform.parent = null;
+            this.gameObject.GetComponent<CapsuleCollider>().enabled = true;
+            this.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            this.gameObject.GetComponent<Rigidbody>().useGravity = true;
+            this.gameObject.GetComponent<SphereCollider>().enabled = true;
+        }
+    }
+
 }
