07319f34048372fc566ec031c19f0649ab67c69c
snowman20TI048 <snowman2000y@gmail.com>
2022-12-20 12:19:16 +0900

    敵と味方のポイントを区別した

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -9,30 +9,46 @@ public class BallController : MonoBehaviour
     [SerializeField]
     private Text PointLabel;
 
-    public int PointCount;
+    public int PlayerPointCount;
+    public int EnemyPointCount;
 
-    [SerializeField, Header("Linecast用 地面判定レイヤー")]
-    private LayerMask pointLayer;
-    public bool isPointed = false;
+    [SerializeField, Header("Linecast用 味方陣地判定レイヤー")]
+    private LayerMask playerArea;
+    [SerializeField, Header("Linecast用 敵陣地判定レイヤー")]
+    private LayerMask enemyArea;
+
+    public bool isPlayerArea = false;
+    public bool isEnemyArea = false;
+
 
     public Radar radarScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        PointLabel.text = "ポイント：" + PointCount;
+        PointLabel.text = "自分のポイント：" + PlayerPointCount
+            + "\n相手のポイント：" + EnemyPointCount;
     }
 
     // Update is called once per frame
     void Update()
     {
         // 地面接地  Physics2D.Linecastメソッドを実行して、Ground Layerとキャラのコライダーとが接地している距離かどうかを確認し、接地しているなら true、接地していないなら false を戻す
-        isPointed = Physics.Linecast(transform.position + transform.up * 0.4f, transform.position - transform.up * 0.9f, pointLayer);
+        isPlayerArea = Physics.Linecast(transform.position + transform.up * 0.4f, transform.position - transform.up * 0.9f, playerArea);
+        isEnemyArea = Physics.Linecast(transform.position + transform.up * 0.4f, transform.position - transform.up * 0.9f, enemyArea);
 
-        if (isPointed == true && radarScript.isChild == false)
+        if (isPlayerArea == true && radarScript.isChild == false)
         {
-            PointCount++;
-            PointLabel.text = "ポイント：" + PointCount;
+            PlayerPointCount++;
+            PointLabel.text = "自分のポイント：" + PlayerPointCount
+             + "\n相手のポイント：" + EnemyPointCount;
+        }
+
+        if (isEnemyArea == true && radarScript.isChild == false)
+        {
+            EnemyPointCount++;
+            PointLabel.text = "自分のポイント：" + PlayerPointCount
+             + "\n相手のポイント：" + EnemyPointCount;
         }
     }
 }
