736d14bce36ff3cd6d04f94e1c242870ff5b549c
snowman20TI048 <snowman2000y@gmail.com>
2022-12-24 10:49:43 +0900

    制限時間の実装と、BGMの設置

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,8 +12,15 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private Text PointLabel;
 
+
+    [SerializeField]
+    private Text TimeLabel;
+
     public int PlayerPointCount;
     public int EnemyPointCount;
+
+    [Header("制限時間")]
+    public float timer;
 
 
     // Start is called before the first frame update
@@ -25,7 +32,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CountDown();
     }
 
     public void AddPlayerPointCount()
@@ -50,4 +57,18 @@ public class GameManager : MonoBehaviour
         drumstick.GetComponent<BallController>().SetUpBall(this);
     }
 
+    private void CountDown()
+    {
+        //時間をカウントダウンする
+        timer -= Time.deltaTime;
+
+        //時間を表示する
+        TimeLabel.text  = timer.ToString("f1") + "秒";
+
+        //countdownが0以下になったとき
+        if (timer <= 0)
+        {
+            TimeLabel.text = "0秒";
+        }
+    }
 }
