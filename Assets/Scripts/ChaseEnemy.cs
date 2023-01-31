7861d6b315c0660a87cbb2d9bfd7ca158581ca1b
snowman20TI048 <snowman2000y@gmail.com>
2023-01-31 22:45:04 +0900

    タイトルシーン、リザルトシーンの作成

--- Assets/Scripts/ChaseEnemy.cs
+++ Assets/Scripts/ChaseEnemy.cs
@@ -6,36 +6,69 @@ using UnityEngine.AI; //これが必要
 public class ChaseEnemy : MonoBehaviour
 {
     
-    public GameObject target;
+    public GameObject target;  //このターゲットは肉のことです。
+    [SerializeField]
+    private GameObject goalObject;
     private NavMeshAgent agent;
     private Animator animator;
+
+    public enum ENEMY_STATE_TYPE
+    {
+        OWNER_CATCH,       //敵が肉を持っている状態
+        OPPONENT_CATCH,      //自分が肉を持っている状態
+        EMPTY,             //誰も肉を持っていない状態
+    }
+
+    public ENEMY_STATE_TYPE enemy_state_type;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         TryGetComponent(out animator);
+        enemy_state_type = ENEMY_STATE_TYPE.EMPTY;
     }
 
     void Update()
     {
-        if (target != null)
+        if (enemy_state_type == ENEMY_STATE_TYPE.OWNER_CATCH)
         {
-            // ターゲットの位置を目的地に設定する。
-            agent.destination = target.transform.position;
+            // ゴールオブジェクトの位置を目的地に設定する。
+            agent.destination = goalObject.transform.position;
+            //距離が0になったら肉を落とす。
+            if ((transform.position - goalObject.transform.position).sqrMagnitude < 1.0)
+            {
+                target.GetComponent<Radar>().Drop();
+            }
+        }
+        else if (enemy_state_type == ENEMY_STATE_TYPE.OPPONENT_CATCH)
+        {
 
-           
-            if (agent.velocity != Vector3.zero)
+        }
+        else if (enemy_state_type == ENEMY_STATE_TYPE.EMPTY)
+        {
+
+            if (target != null)
             {
-                animator.SetFloat("Run", 0.3f);
+                // ターゲットの位置を目的地に設定する。
+                agent.destination = target.transform.position;
+
+
+
+
             }
-            else
-            {
-                animator.SetFloat("Run", 0);
-            }
-            
+
         }
 
-        
+        //移動していたら
+        if (agent.velocity != Vector3.zero)
+        {
+            animator.SetFloat("Run", 0.3f);
+        }
+        else
+        {
+            animator.SetFloat("Run", 0);
+        }
+
     }
 
 }
--- Assets/Scripts/Radar.cs
+++ Assets/Scripts/Radar.cs
@@ -12,6 +12,11 @@ public class Radar : MonoBehaviour
     // 「OnTriggerStay」はトリガーが他のコライダーに触れている間中実行されるメソッド（ポイント）
     private void OnTriggerStay(Collider other)
     {
+        if (isChild)
+        {
+            return;
+        }
+
         // もしも他のオブジェクトに「Player」というTag（タグ）が付いていたならば（条件）
         if (other.CompareTag("Player"))
         {
@@ -33,6 +38,32 @@ public class Radar : MonoBehaviour
             }
 
         }
+
+        if (other.CompareTag("Enemy"))
+        {
+
+            if (isChild == false)
+            {
+                Debug.Log("a");
+                this.gameObject.GetComponent<Rigidbody>().useGravity = false;
+                this.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                this.gameObject.GetComponent<CapsuleCollider>().enabled = false;
+                this.gameObject.transform.parent = other.gameObject.transform;
+                this.gameObject.transform.localPosition = new Vector3(0f, 0.07f, 0f);
+                this.gameObject.GetComponent<SphereCollider>().enabled = false;
+                isChild = true;
+                if (other.TryGetComponent(out ChaseEnemy chaseEnemy) == true)
+                {
+                    chaseEnemy.enemy_state_type = ChaseEnemy.ENEMY_STATE_TYPE.OWNER_CATCH;
+                }
+
+
+            }
+
+
+            
+
+        }
     }
 
     private void Update()
@@ -43,15 +74,20 @@ public class Radar : MonoBehaviour
         }
         if(Input.GetKeyDown(KeyCode.C) && isChild == true)
         {
-            Debug.Log("b");
-            isChild = false;
-            this.gameObject.transform.localPosition = new Vector3(0f, 0.07f, 0.07f);
-            this.gameObject.transform.parent = null;
-            this.gameObject.GetComponent<CapsuleCollider>().enabled = true;
-            this.gameObject.GetComponent<Rigidbody>().isKinematic = false;
-            this.gameObject.GetComponent<Rigidbody>().useGravity = true;
-            this.gameObject.GetComponent<SphereCollider>().enabled = true;
+            Drop();
         }
+    }
+
+    public void Drop()
+    {
+        Debug.Log("b");
+        isChild = false;
+        this.gameObject.transform.localPosition = new Vector3(0f, 0.07f, 0.07f);
+        this.gameObject.transform.parent = null;
+        this.gameObject.GetComponent<CapsuleCollider>().enabled = true;
+        this.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        this.gameObject.GetComponent<Rigidbody>().useGravity = true;
+        this.gameObject.GetComponent<SphereCollider>().enabled = true;
     }
 
 }
